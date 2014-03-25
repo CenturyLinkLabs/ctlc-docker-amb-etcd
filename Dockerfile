5cd37b2991a1a003474296eeb32134d8f2f0bf61
Lucas Carlson <lucas@rufy.com>
2014-03-25 00:34:48 -0700

    Removing socat

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ MAINTAINER	lucas@rufy.com
 
 ADD https://github.com/coreos/etcd/releases/download/v0.3.0/etcd-v0.3.0-linux-amd64.tar.gz /etcd.tgz
 ADD run.sh /run.sh
+ADD tcp-proxy /usr/bin
 
 RUN tar xfz etcd.tgz
 RUN mv etcd-*/etcdctl /usr/bin
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
-env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat TCP4-LISTEN:\1,fork,reuseaddr TCP4:\2:\3 \&/'  | sh 
+env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LISTEN_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > env
 
-env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export PORT=\1/' | sh 
+source env
 
-while true; do etcdctl -C "http://172.17.42.1:4001/" set /services/${NAME:-$PORT} "{ \"host\": \"%H\", \"port\": $PORT, \"version\": \"${VERSION:-1}\" }" --ttl 60;sleep 45;done
+tcp-proxy $LISTEN_PORT $REMOTE_HOST $REMOTE_PORT &
+
+while true
+do
+	etcdctl -C "http://172.17.42.1:4001/" set \
+		/services/${NAME:-$LISTEN_PORT} \
+		"{ \"host\": \"%H\", \"port\": $LISTEN_PORT, \"version\": \"${VERSION:-1}\" }" --ttl 60
+	sleep 45
+done
