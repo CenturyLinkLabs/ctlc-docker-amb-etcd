e71655882ea8aa0ed7ce52cd9eda8dac1fa670e0
Lucas Carlson <lucas@rufy.com>
2014-03-25 00:48:12 -0700

    Fixing hostname

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LISTEN_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > env
+env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export LOCAL_PORT=\1\nexport REMOTE_HOST=\2\nexport REMOTE_PORT=\3/' > env
 
 source env
+export LOCAL_HOST=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 
-tcp-proxy $LISTEN_PORT $REMOTE_HOST $REMOTE_PORT &
+tcp-proxy $LOCAL_PORT $REMOTE_HOST $REMOTE_PORT &
 
 while true
 do
 	etcdctl -C "http://172.17.42.1:4001/" set \
 		/services/${NAME:-$LISTEN_PORT} \
-		"{ \"host\": \"%H\", \"port\": $LISTEN_PORT, \"version\": \"${VERSION:-1}\" }" --ttl 60
+		"{ \"host\": \"$HOSTNAME\", \"ip\": \"$LOCAL_HOST\", \"port\": $LISTEN_PORT, \"version\": \"${VERSION:-1}\" }" --ttl 60
 	sleep 45
 done
