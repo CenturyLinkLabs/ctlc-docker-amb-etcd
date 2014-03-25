a2ebfe6704608c54d4cc6a7d9e4718a11362af64
Lucas Carlson <lucas@rufy.com>
2014-03-24 23:18:12 -0700

    let etcdctl talk to etcd

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat TCP
 
 env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/export PORT=\1/' | sh 
 
-while true; do etcdctl set /services/${NAME:-$PORT} "{ \"host\": \"%H\", \"port\": $PORT, \"version\": \"${VERSION:-1\" }" --ttl 60;sleep 45;done
+while true; do etcdctl -C "http://172.17.42.1:4001/" set /services/${NAME:-$PORT} "{ \"host\": \"%H\", \"port\": $PORT, \"version\": \"${VERSION:-1\" }" --ttl 60;sleep 45;done
